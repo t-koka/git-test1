0694e673db96345117ef0408071339e4b57e7c4a
Tejal Koka <tejalkoka@Tejals-MacBook-Air.local>
2023-09-07 13:48:22 -0400

    Initial Files

---
+++ uncc.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UNCC-TEJAL KOKA</title>
+</head>
+<body>
+    <h1> I am testing the page </h1>
+    <h1> Tejal Koka </h1>
+    <h2> UNCC </h2>
+
+    <?php echo 9 * 13 ?>
+
+</body>
+</html>
